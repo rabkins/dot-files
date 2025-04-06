3e71ed29dd4fdf8d9f77f6a5702f374d5fc42542
Marcin Raba <rabkins@Marcins-MacBook-Air.local>
2025-04-06 07:43:13 +0200

    remove custom folding

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,15 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
-opt.foldlevel = 20
-opt.foldmethod = "expr"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
-opt.foldcolumn = "1"
-opt.foldlevelstart = 1
-opt.foldenable = false
-
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 
